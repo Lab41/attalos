d212ff5be5ea5d9726fbaa3411e53a8ef446189a
Yonas Tesfaye <ymt123@alum.mit.edu>
2016-05-26 17:25:45 -0700

    Additional bug fixes around extracting files (and adding image content)

--- attalos/dataset/mscoco_prep.py
+++ attalos/dataset/mscoco_prep.py
@@ -11,7 +11,7 @@ from dataset_prep import DatasetPrep, RecordMetadata
 
 TRAIN_VAL_INSTANCES_2014_URL = 'http://msvocds.blob.core.windows.net/annotations-1-0-3/instances_train-val2014.zip'
 TRAIN_VAL_IMAGE_CAPTIONS_2014_URL = 'http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip'
-
+TRAIN_IMAGE_2014_URL = 'http://msvocds.blob.core.windows.net/coco2014/train2014.zip'
 
 class MSCOCODatasetPrep(DatasetPrep):
     def __init__(self, dataset_directory):
@@ -19,12 +19,14 @@ class MSCOCODatasetPrep(DatasetPrep):
         self.dataset_directory = dataset_directory
         self.instances_filename = self.get_candidate_filename(TRAIN_VAL_INSTANCES_2014_URL)
         self.caption_filename = self.get_candidate_filename(TRAIN_VAL_IMAGE_CAPTIONS_2014_URL)
+        self.image_filename = self.get_candidate_filename(TRAIN_IMAGE_2014_URL)
         self.download_dataset()
         self.item_info = self.load_metadata()
 
     def download_dataset(self):
         self.download_if_not_present(self.instances_filename, TRAIN_VAL_INSTANCES_2014_URL)
         self.download_if_not_present(self.caption_filename, TRAIN_VAL_IMAGE_CAPTIONS_2014_URL)
+        self.image_filename = self.get_candidate_filename(TRAIN_IMAGE_2014_URL)
 
     def load_metadata(self):
         with zipfile.ZipFile(self.caption_filename) as input_file:
@@ -65,14 +67,15 @@ class MSCOCODatasetPrep(DatasetPrep):
 
 
     def extract_image_by_key(self, key):
-        key_info = self.get_key(key)
-        with zipfile.ZipFile(self.data_fname) as input_file:
+        key_info = key.get_key(key)
+        with zipfile.ZipFile(self.image_filename) as input_file:
             train_captions = input_file.open('train2014/%s'%key_info.image_name)
             return train_captions.read()
+        raise FileNotFoundError('Unable to extract %s'%key_info.image_name)
 
 
     def extract_image_to_location(self, key, desired_file_path):
-        fOut = open(desired_file_path, 'w')
+        fOut = open(desired_file_path, 'wb')
         fOut.write(self.extract_image_by_key(key))
         fOut.close()
 
@@ -97,10 +100,5 @@ class MSCOCODatasetPrep(DatasetPrep):
 
     def list_keys(self):
         return self.item_info.keys()
-
-
-
-t = MSCOCODatasetPrep('/data/mscoco')
-print(t.load_captions()[524286])
 
 
