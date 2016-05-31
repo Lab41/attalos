f929ea00a62c843628263a1e58e5cf7997234519
kni <kni@iqt.org>
2016-05-31 16:21:41 -0700

    Current version of image preparation for VG. Runs fairly slowly

--- attalos/dataset/vg_prep.py
+++ attalos/dataset/vg_prep.py
@@ -12,7 +12,7 @@ import sys
 # Import the Visual Genome API for functionality
 VISUAL_GENOME_API = '/work/attalos/accessVG'
 
-VISUAL_GENOME_IMAGES = 'https://visualgenome.org/static/data/dataset/image_data.json.zip'
+VISUAL_GENOME_IMAGES = 'https://cs.stanford.edu/people/rak248/VG_100K/images.zip'
 VISUAL_GENOME_METADATA = 'https://visualgenome.org/static/data/dataset/image_data.json.zip'
 VISUAL_GENOME_REGIONS = 'https://visualgenome.org/static/data/dataset/region_descriptions.json.zip'
 VISUAL_GENOME_OBJECTS = 'https://visualgenome.org/static/data/dataset/objects.json.zip'
@@ -50,38 +50,45 @@ class VGDatasetPrep(DatasetPrep):
 
 	self.data_dir = dataset_directory
 
-        self.relationships_filename = self.get_candidate_filename(VISUAL_GENOME_RELATIONSHIPS)
-        self.metadata_filename = self.get_candidate_filename(VISUAL_GENOME_METADATA)
-        self.images_filename = self.get_candidate_filename(VISUAL_GENOME_IMAGES)
+	self.relationships_filename = self.get_candidate_filename(VISUAL_GENOME_RELATIONSHIPS)
+	self.metadata_filename = self.get_candidate_filename(VISUAL_GENOME_METADATA)
+	self.images_filename = self.get_candidate_filename(VISUAL_GENOME_IMAGES)
 	self.objects_filename = self.get_candidate_filename(VISUAL_GENOME_OBJECTS)
 	self.attributes_filename = self.get_candidate_filename(VISUAL_GENOME_ATTRIBUTES)
-        # self.download_dataset()
+        self.download_dataset()
 	self.load_metadata()
 	self.images_file_handle = None
 
-    def download_dataset(self):
+    def download_dataset(self, extract_all_images=False):
         """
         Downloads the dataset if it's not already present in the download directory
         Returns:
-
         """
-        self.download_if_not_present(self.metadata_filename, VISUAL_GENOME_METADATA)
-        self.download_if_not_present(self.images_filename, VISUAL_GENOME_IMAGES )
+
+	self.download_if_not_present(self.metadata_filename, VISUAL_GENOME_METADATA)
+	self.download_if_not_present(self.images_filename, VISUAL_GENOME_IMAGES )
         self.download_if_not_present(self.relationships_filename, VISUAL_GENOME_RELATIONSHIPS)
         self.download_if_not_present(self.objects_filename, VISUAL_GENOME_OBJECTS)
         self.download_if_not_present(self.attributes_filename, VISUAL_GENOME_ATTRIBUTES)
-       
-	zipref = zipfile.ZipFile(self.metadata_filename,'r')
-	zipref.extractall(data_dir)
-	zipref = zipfile.ZipFile(self.images_filename,'r')
-	zipref.extractall(data_dir)
-        zipref = zipfile.ZipFile(self.relationships_filename,'r')
-        zipref.extractall(data_dir)
-        zipref = zipfile.ZipFile(self.objects_filename,'r')
-        zipref.extractall(data_dir)
-        zipref = zipfile.ZipFile(self.attributes_filename,'r')
-        zipref.extractall(data_dir)
-	
+
+	import os
+	if not os.path.exists(self.metadata_filename[:-4]):
+		zipref = zipfile.ZipFile(self.metadata_filename,'r')
+		zipref.extractall(self.data_dir)
+	if not os.path.exists(self.relationships_filename[:-4]):
+        	zipref = zipfile.ZipFile(self.relationships_filename,'r')
+        	zipref.extractall(self.data_dir)
+	if not os.path.exists(self.objects_filename[:-4]):
+        	zipref = zipfile.ZipFile(self.objects_filename,'r')
+        	zipref.extractall(self.data_dir)
+	if not os.path.exists(self.attributes_filename[:-4]):
+        	zipref = zipfile.ZipFile(self.attributes_filename,'r')
+        	zipref.extractall(self.data_dir)
+
+	if extract_all_images:
+		zipref = zipfile.ZipFile(self.images_filename,'r')
+		zipref.extractall(data_dir)
+
 
     def load_metadata(self):
         """
@@ -127,9 +134,12 @@ class VGDatasetPrep(DatasetPrep):
         key_info = self.get_key(key)
 
         if self.images_file_handle is None:
-            self.images_file_handle = zipfile.ZipFile('/data/fs4/datasets/visual_genome/images.zip')
+            self.images_file_handle = zipfile.ZipFile(self.images_filename)
 
+	zipfile.ZipFile.namelist(self.images_file_handle)
+	print('image name %s'%(self.images_filename))
         train_captions = self.images_file_handle.open('%s'%imname)
+
         return train_captions.read()
 
     def extract_image_to_location(self, key, desired_file_path):
