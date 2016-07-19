b5de21ca2eafa949183145f6e47452f37d581312
cfoster@lab41.org <cfoster@DEV-LT-5637.local>
2016-07-19 13:59:42 -0700

    Documentation and removing unnecessary tests

--- attalos/evaluation/evaluation.py
+++ attalos/evaluation/evaluation.py
@@ -2,21 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import math
 import numpy as np
 import scipy as sp
 
 from sklearn import metrics
 
-from oct2py import octave
-
 class Eval(object):
     """
     Assumes:
-        predicted: numpy matrix of category prediction confidence [trial, tag]
-        eval_data: numpy matrix of ground truth labeling [trial, tag]
-
-        precision and roc_auc cannot be evaluated if a label has no true samples
+        predicted: matrix of label prediction confidence [trial, tag]
+        eval_data: matrix of ground truth classifications [trial, tag]
     """
     def __init__(self, truth, predictions):            
         self.predictions_raw = predictions
@@ -28,8 +23,9 @@ class Eval(object):
         self.ntrials = predictions.shape[0]
         self.ntags = predictions.shape[1]
 
-        self.metrics = [self.m_precision, self.m_recall, self.coverage_error, 
-                        self.ranking_precision, self.ranking_loss, self.roc_auc]
+        self.metrics = [self.precision, self.recall, self.f1, 
+                        self.coverage_error, self.ranking_precision, 
+                        self.ranking_loss, self.roc_auc]
 
     def top_k(self, k):
         if k <= 0:
@@ -42,9 +38,9 @@ class Eval(object):
 
         predictions = np.zeros(self.predictions_raw.shape)
 
-        for raw_row, prediction_row in zip(self.predictions_raw, predictions):
-            top_indices = np.argsort(raw_row)[-int(k):]
-            prediction_row[top_indices] = 1
+        for raw_r, prediction_r in zip(self.predictions_raw, predictions):
+            top_indices = np.argsort(raw_r)[-int(k):]
+            prediction_r[top_indices] = 1
 
         self.predictions = predictions.astype(int)
         self.k = k
@@ -56,109 +52,110 @@ class Eval(object):
         self.k = threshold
         return temp
 
-    def print_evaluation(self):
-        print('---Evaluation---')
-        if self.k >= 1:
-            print('---(where k = ' + str(self.k) + ')---')
-        else:
-            print('---where confidence > ' + str(self.k) + ' is classified as positive---')
-        for metric in self.metrics:
-            print(metric())
-
-    def m_precision(self):
+    def precision(self):
         """
         Unweighted precision score
         """
         try:
-            self.m_precision = metrics.precision_score(self.ground_truth, self.predictions, average='macro')
+            self.m_precision = metrics.precision_score(
+                self.ground_truth, self.predictions, 
+                average='macro')
         except UndefinedMetricWarning:
             pass
         return 'Precision: ' + str(self.m_precision)
 
-    def w_precision(self):
-        """
-        Weighted precision score, requires appearance of each label in the ground truth set
-        """
-        try:
-            self.w_precision = metrics.precision_score(self.ground_truth, self.predictions, average='weighted')
-        except UndefinedMetricWarning:
-            pass
-        return 'Precision (weighted): ' + str(self.w_precision)
-
-    def m_recall(self):
+    def recall(self):
         """
         Unweighted recall score
         """
         try:
-            self.m_recall = metrics.recall_score(self.ground_truth, self.predictions, average='macro')
+            self.m_recall = metrics.recall_score(
+                self.ground_truth, self.predictions, 
+                average='macro')
         except UndefinedMetricWarning:
             pass
         return 'Recall: ' + str(self.m_recall)
 
-    def w_recall(self):
-        """
-        Weighted recall score
-        """
-        try:
-            self.w_recall = metrics.recall_score(self.ground_truth, self.predictions, average='weighted')
-        except UndefinedMetricWarning:
-            pass
-        return 'Recall (weighted): ' + str(self.w_recall)
-
     def f1(self):
-        self.f1 = metrics.f1_score(self.ground_truth, self.predictions, average='micro')
+        self.f1 = metrics.f1_score(
+            self.ground_truth, self.predictions, 
+            average='micro')
         return 'F1: ' + str(self.f1)
 
     def roc_auc(self):
         """
         Assumes:
-            each column has at least two values (i.e. each example tag appears more than once)
+            each column has at least two values 
+            (i.e. each example tag appears more than once)
         """
         try:
-            self.roc_auc = metrics.roc_auc_score(self.ground_truth, self.predictions_raw, average='macro')
-            return 'Area Under Curve [0, 1, where 0.5 = chance]: ' + str(self.roc_auc)
+            self.roc_auc = metrics.roc_auc_score(
+                self.ground_truth, self.predictions_raw, 
+                average='macro')
+            return 'AUC: ' + str(self.roc_auc)
         except ValueError:
             return 'Area Under Curve could not be computed ...'
 
     def coverage_error(self):
-        self.coverage_error = metrics.coverage_error(self.ground_truth, self.predictions_raw)
+        """
+        The coverage_error function computes the average number of labels 
+        that have to be included in the final prediction such that all true 
+        labels are predicted. This is useful if you want to know how many 
+        top-scored-labels you have to predict in average without missing any 
+        true one. The best value of this metrics is thus the average number 
+        of true labels.
+        """
+        self.coverage_error = metrics.coverage_error(
+            self.ground_truth, self.predictions_raw)
         avg_true_labels = np.count_nonzero(self.ground_truth) / self.ntrials
-        return 'Coverage Error [' + str(avg_true_labels) + ', ~): ' + str(self.coverage_error)
+        ce_message = 'Coverage Error [' + str(avg_true_labels) + ', ~): '
+        return ce_message + str(self.coverage_error)
 
     def ranking_precision(self):
-        self.ranking_precision = metrics.label_ranking_average_precision_score(self.ground_truth, self.predictions_raw)
-        return 'Ranking Precision (0, 1]: ' + str(self.ranking_precision)
+        """
+        Label ranking average precision (LRAP) is the average over each 
+        ground truth label assigned to each sample, of the ratio of 
+        true vs. total labels with lower score. This metric will yield 
+        better scores if you are able to give better rank to the labels 
+        associated with each sample. The obtained score is always strictly 
+        greater than 0, and the best value is 1.
+        """
+        self.ranking_precision = metrics.label_ranking_average_precision_score(
+            self.ground_truth, self.predictions_raw)
+        rp_message = 'Ranking Precision (0, 1]: '
+        return rp_message + str(self.ranking_precision)
 
     def ranking_loss(self):
-        self.ranking_loss = metrics.label_ranking_loss(self.ground_truth, self.predictions_raw)
-        return 'Ranking Loss: ' + str(self.ranking_loss)
-
-    def msq_error(self):
-        scores = np.empty((self.ntrials, 1))
-        for image_n in range(0, self.ntrials):
-            score = metrics.mean_squared_error(self.ground_truth[image_n], self.predictions_raw[image_n])
-            scores[image_n] = score
-        self.msq_error = np.average(scores)
-        return 'Mean Squared Error: ' + str(self.msq_error)
+        self.ranking_loss = metrics.label_ranking_loss(
+            self.ground_truth, self.predictions_raw)
+        rl_message = 'Ranking Loss: '
+        return rl_message + str(self.ranking_loss)
 
     def spearman(self):
         scores = np.empty((self.ntags, 1))
         for tag_n in range(0, self.ntags):
-            [spearman_value, p_value] = sp.stats.spearmanr(self.ground_truth[:,tag_n], self.predictions_raw[:,tag_n])
+            [spearman_value, p_value] = sp.stats.spearmanr(
+                self.ground_truth[:,tag_n], self.predictions_raw[:,tag_n])
             if (math.isnan(spearman_value)):
                 spearman_value = 0.0
             scores[tag_n] = spearman_value
 
         self.spearman = np.average(scores)
-        return 'Average Spearman\'s coefficient is: ' + str(self.spearman)
+        s_message = 'Average Spearman\'s coefficient is: '
+        return s_message + str(self.spearman)
 
-    def kendall_tau(self):
-        scores = np.empty((self.ntrials, 1)) 
-        for image_n in range(0, self.ntrials):
-            [kt_value, p_value] = sp.stats.kendalltau(self.ground_truth[image_n], self.predictions_raw[image_n])
-            scores[image_n] = kt_value
-        self.kendall_tau = np.average(scores)
-        return 'Kendall\'s tau [-1, 1]: ' + str(self.kendall_tau)
+    def print_evaluation(self):
+        print('---Evaluation---')
+        if self.k >= 1:
+            print('---(where k = ' 
+                  + str(self.k) 
+                  + ')---')
+        else:
+            print('---where confidence > ' 
+                  + str(self.k) 
+                  + ' is classified as positive---')
+        for metric in self.metrics:
+            print(metric())
 
 
 def main():
