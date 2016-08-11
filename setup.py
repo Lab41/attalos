17e2c8933fde0278c0a31b24176c4b7a4e815213
kylemvz <kylez@lab41.org>
2016-08-11 18:07:48 +0000

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(name='Attalos',
+      version='0.1',
+      description='Lab41 Joint Vector Space Challenge',
+      author='Lab41',
+      author_email='vsandesara@iqt.org',
+      url='https://github.com/Lab41/attalos',
+      packages=find_packages(),
+     )
