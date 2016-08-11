9623dc64edadc421d1ade995b3ce69e8dcc236d8
kylemvz <kylez@lab41.org>
2016-08-11 17:45:44 +0000

    Added new scaling function. Updated function signatures for construct_W and broadcast_transform.

--- attalos/imgtxt_algorithms/correlation/correlation.py
+++ attalos/imgtxt_algorithms/correlation/correlation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def construct_W(w2v_model, vocab):
+def construct_W(w2v_model, vocab, dtype=np.float64):
     """
     Creates and returns a numpy ndarray of dimensions (200 x size(vocab)).
     The nth column in this array represents the Word2Vec vector for the nth word in vocab.
@@ -13,7 +13,7 @@ def construct_W(w2v_model, vocab):
     Returns:
         a numpy ndarray
     """
-    return np.asarray([w2v_model.get_vector(word) for word in vocab if word in w2v_model.vocab]).T
+    return np.asarray([w2v_model.get_vector(word) for word in vocab if word in w2v_model.vocab], dtype=dtype).T
 
 def get_invalid_labels(labels, valid_labels):
     """
@@ -29,6 +29,25 @@ def get_invalid_labels(labels, valid_labels):
     """
     return {word: idx for (idx, word) in enumerate(labels) if word not in valid_labels}
 
+def broadcast_transform(arr, v):
+    """
+    Transforms v so that it is broadcastable to arr in a row-wise fashion.
+    For instance, if v = [1, 2, 3, 4], and arr is a 3x4 numpy ndarray, then the return value is:
+    [
+        [1, 1, 1, 1],
+        [2, 2, 2, 2],
+        [3, 3, 3, 3],
+        [4, 4, 4, 4]
+    ]
+
+    Args:
+        v: a numpy vector (1d ndarray)
+
+    Returns:
+        a numpy ndarray
+    """
+    return v[np.newaxis].T.repeat(arr.shape[1]).reshape(arr.shape)
+
 def scale(arr):
     """
     Returns a new numpy ndarray where the nth row is the scaled version of the nth row of arr.
@@ -40,28 +59,24 @@ def scale(arr):
     Returns:
         a numpy ndarray
     """
-    def broadcast_transform(v):
-        """
-        Transforms v so that it is broadcastable to arr in a row-wise fashion.
-        For instance, if v = [1, 2, 3, 4], and arr is a 3x4 numpy ndarray, then the return value is:
-        [
-            [1, 1, 1, 1],
-            [2, 2, 2, 2],
-            [3, 3, 3, 3],
-            [4, 4, 4, 4]
-        ]
-
-        Args:
-            v: a numpy vector (1d ndarray)
-
-        Returns:
-            a numpy ndarray
-        """
-        return v[np.newaxis].T.repeat(arr.shape[1]).reshape(arr.shape)
-
-    broadcast_min = broadcast_transform(np.amin(arr, 1))
-    broadcast_max = broadcast_transform(np.amax(arr, 1))
+    broadcast_min = broadcast_transform(arr, np.amin(arr, 1))
+    broadcast_max = broadcast_transform(arr, np.amax(arr, 1))
     return (arr - broadcast_min) / (broadcast_max - broadcast_min)
+
+def scale2(arr):
+    """
+    Returns a new numpy ndarray where the nth row is the scaled2 version of the nth row of arr.
+    Each row is scaled such that it has mean 0 and stdev 1.
+    
+    Args:
+        arr: a numpy ndarray
+        
+    Returns:
+        a numpy ndarray
+    """
+    broadcast_mean = broadcast_transform(arr, np.mean(arr, axis=1))
+    broadcast_std = broadcast_transform(arr, np.std(arr, axis=1))
+    return (arr - broadcast_mean) / broadcast_std
 
 def nonlinearity(arr, coef=-1, offset=1, power=2, alpha=0.005):
     """
