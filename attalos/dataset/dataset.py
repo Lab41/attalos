65a401ce3beea266d2d74e1bc511aaf9b184d822
Yonas Tesfaye <ymt123@alum.mit.edu>
2016-09-25 00:33:32 +0000

    Allow usage of ujson if available

--- attalos/dataset/dataset.py
+++ attalos/dataset/dataset.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import json
+try:
+    import ujson as json
+except:
+    import json
 import gzip
 
 import numpy as np
